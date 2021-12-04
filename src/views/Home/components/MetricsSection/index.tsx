bc5630e9fa40e5aac0efa01e2448116314b1495a
muutualswap <75495839+muutualswap@users.noreply.github.com>
2021-12-03 19:18:25 -0500

    Update index.tsx

--- src/views/Home/components/MetricsSection/index.tsx
+++ src/views/Home/components/MetricsSection/index.tsx
@@ -25,38 +25,38 @@ const Stats = () => {
   const [entrusting, inFunds] = tvlText.split(tvlString)
 
   const UsersCardData: IconCardData = {
-    icon: <CommunityIcon color="secondary" width="36px" />,
+    icon: <CommunityIcon color="blue" width="36px" />,
   }
 
   const TradesCardData: IconCardData = {
-    icon: <SwapIcon color="primary" width="36px" />,
+    icon: <SwapIcon color="blue" width="36px" />,
   }
 
   const StakedCardData: IconCardData = {
-    icon: <ChartIcon color="failure" width="36px" />,
+    icon: <ChartIcon color="blue" width="36px" />,
   }
 
   return (
     <Flex justifyContent="center" alignItems="center" flexDirection="column">
       <GradientLogo height="48px" width="48px" mb="24px" />
       <Heading textAlign="center" scale="xl">
-        {t('Used by millions.')}
+        {t('Soon to be Used by millions.')}
       </Heading>
       <Heading textAlign="center" scale="xl" mb="32px">
-        {t('Trusted with billions.')}
+        {t('On the way to being trusted with billions.')}
       </Heading>
-      <Text textAlign="center" color="textSubtle">
-        {t('PancakeSwap has the most users of any decentralized platform, ever.')}
+      <Text textAlign="center" color="blue">
+        {t('BigSwap will have the best users of any decentralized platform, ever.')}
       </Text>
       <Flex flexWrap="wrap">
-        <Text display="inline" textAlign="center" color="textSubtle" mb="20px">
+        <Text display="inline" textAlign="center" color="blue" mb="20px">
           {entrusting}
           <>{data ? <>{tvlString}</> : <Skeleton display="inline-block" height={16} width={70} mt="2px" />}</>
           {inFunds}
         </Text>
       </Flex>
 
-      <Text textAlign="center" color="textSubtle" bold mb="32px">
+      <Text textAlign="center" color="blue" bold mb="32px">
         {t('Will you join them?')}
       </Text>
 
@@ -79,7 +79,7 @@ const Stats = () => {
           <StatCardContent
             headingText={t('$%tvl% staked', { tvl: tvlString })}
             bodyText={t('Total Value Locked')}
-            highlightColor={theme.colors.failure}
+            highlightColor={theme.colors.success}
           />
         </IconCard>
       </Flex>
