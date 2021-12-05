ace62bbde3ddace664ed58b37a8409e861b93155
muutualswap <75495839+muutualswap@users.noreply.github.com>
2021-12-04 22:24:30 -0500

    Update IfoCardTokens.tsx

--- src/views/Ifos/components/IfoFoldableCard/IfoPoolCard/IfoCardTokens.tsx
+++ src/views/Ifos/components/IfoFoldableCard/IfoPoolCard/IfoCardTokens.tsx
@@ -95,7 +95,7 @@ const IfoCardTokens: React.FC<IfoCardTokensProps> = ({
   const { t } = useTranslation()
   const { targetRef, tooltip, tooltipVisible } = useTooltip(
     t(
-      'Sorry, you didn’t contribute enough CAKE to meet the minimum threshold. You didn’t buy anything in this sale, but you can still reclaim your CAKE.',
+      'Sorry, you didn’t contribute enough BGSP to meet the minimum threshold. You didn’t buy anything in this sale, but you can still reclaim your BGSP.',
     ),
     { placement: 'bottom' },
   )
@@ -113,9 +113,9 @@ const IfoCardTokens: React.FC<IfoCardTokensProps> = ({
     }
     if (account && !hasProfile) {
       if (publicIfoData.status === 'finished') {
-        return <Text textAlign="center">{t('Activate PancakeSwap Profile to take part in next IFO‘s!')}</Text>
+        return <Text textAlign="center">{t('Activate BigSwap Profile to take part in next IFO‘s!')}</Text>
       }
-      return <Text textAlign="center">{t('You need an active PancakeSwap Profile to take part in an IFO!')}</Text>
+      return <Text textAlign="center">{t('You need an active BigSwap Profile to take part in an IFO!')}</Text>
     }
     if (publicIfoData.status === 'coming_soon') {
       return (
