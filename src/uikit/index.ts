a926dc313b92a85970f3d119f6c934fecb7ce389
muutualswap <75495839+muutualswap@users.noreply.github.com>
2021-12-08 17:25:52 -0500

    Update index.ts

--- src/uikit/index.ts
+++ src/uikit/index.ts
@@ -8,6 +8,7 @@ export * from "./components/ButtonMenu";
 export * from "./components/Card";
 export * from "./components/CakePrice";
 export * from "./components/Checkbox";
+export * from "./components/Dropdown";
 export * from "./components/FallingBunnies";
 export * from "./components/Heading";
 export * from "./components/Image";
